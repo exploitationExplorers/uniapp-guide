0c505cdf1e95b490504bfc7718f9da1609d71316
Xin <415272997@qq.com>
2025-11-22 15:17:46 +0800

    项目基本搭建

---
+++ pages/404/404.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		<view class="texts">404</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style scoped>
+    .texts{
+		font-size: 70rpx;
+		color: #111;
+		text-align: center;
+	}
+</style>
---
+++ pages/groupStatistics/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		带团统计
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
